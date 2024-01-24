8b82ca984b09cfafdcede89514fa35ad4145389e
RostamAkman <143599159+RostamAkman@users.noreply.github.com>
2024-01-24 13:50:09 +0100

    Update BMI.java
    Kommenterat kod

--- src/main/java/se/nackademin/BMI.java
+++ src/main/java/se/nackademin/BMI.java
@@ -4,26 +4,36 @@ import java.util.Optional;
 
 //import static se.nackademin.BMI_utils.*;
 
+/**
+ * En klass som kör igenom hela BMI programmet.
+ */
 public class BMI {
 
-
+      /**
+     * Metoden som kör igenom BMI programmet.
+     */
     public static void BMIcalc() {
+        //Frågar om vikt och längd.
         double weight = BMI_utils.getValidNumericInput(Optional.empty(), "Hur mycket väger du (i kg)?: ");
         double heightInCentimeters = BMI_utils.getValidNumericInput(Optional.empty(), "Hur lång är du (i cm)?: ");
 
+        // Konvertera längden till meter för BMI-beräkning
         double heightInMeters = heightInCentimeters / 100.0;
-
+        
+        // Beräkna och skriv ut BMI
         int bmi = (int) BMI_utils.calculateBMI(weight, heightInMeters);
         System.out.println('\n' + "Ditt BMI är: " + bmi);
 
+        // Hämta användarinput för kön, ålder och aktivitetsnivå
         char gender = BMI_utils.getValidGenderInput(Optional.empty());
         int age = BMI_utils.getValidIntegerInput(Optional.empty(), "Ange din ålder: ");
 
+        // Beräkna och skriv ut rekommenderat dagligt kaloriintag baserat på användarinformation
         int activityLevel = BMI_utils.getValidActivityLevelInput();
-
         int suggestedCaloricIntake = BMI_utils.suggestCaloricIntake(gender, age, weight, heightInMeters, activityLevel);
         System.out.println('\n' + "Baserat på din basalmetabolism, aktivitetsnivå och ålder är den rekommenderade kaloriintaget/dag: " + suggestedCaloricIntake + " kal"+'\n');
-
+          
+        // Hämta användarinput för viktrelaterat mål
         int goal = BMI_utils.getValidGoalInput();
 
         if (goal == 1) { //Behålla intag
@@ -33,10 +43,12 @@ public class BMI {
             System.out.println("Ditt dagliga kaloriintag för att öka i vikt: " + caloriesForGain);
 
         } else if (goal == 3) { //Minska intag
-     
+            
+            // Hämta användarinput för viktminskningsmål
             double kgAttGåNer = BMI_utils.getValidNumericInput(Optional.empty(), "Ange hur många kilo du vill gå ner: ");
-            int kalorierAttMinska = BMI_utils.antalerKalorierTillDatum(kgAttGåNer);
 
+            // Beräkna antal kalorier att minska och uppdatera det totala dagliga kaloriintaget
+            int kalorierAttMinska = BMI_utils.antalerKalorierTillDatum(kgAttGåNer);
             int totalaMängdenKalorierMinskning = suggestedCaloricIntake - kalorierAttMinska;
 
             System.out.println("Antal kalorier att minska: " + kalorierAttMinska + " Kalorier"
